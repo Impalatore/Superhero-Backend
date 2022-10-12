063b52c303fa19ed874d5601c816af8e29fc362a
Impalatore <naim7710@gmail.com>
2022-10-12 12:32:28 -0500

    Dotnet EF Initial Migration

---
+++ Superhero Backend/Data/DataContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Superhero_Backend.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+
+    public DbSet<SuperHero> SuperHeroes => Set<SuperHero>();
+}
