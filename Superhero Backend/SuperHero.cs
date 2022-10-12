861a0892b1755fe72f727c6a5d546518aa266e7f
Impalatore <naim7710@gmail.com>
2022-10-12 01:21:46 -0500

    Implementación de CORS

---
+++ Superhero Backend/SuperHero.cs
@@ -0,0 +1,10 @@
+﻿namespace Superhero_Backend;
+
+public class SuperHero
+{
+    public int Id { get; set; }
+    public String Name { get; set; } = String.Empty;
+    public String FirstName { get; set; } = String.Empty;
+    public String LastName { get; set; } = String.Empty;
+    public String Place { get; set; } = String.Empty;
+}
